84e1aa48e9b6d253207027c0171bcc402bb2037a
Redns <ljing431@163.com>
2025-01-02 20:57:57 +0800

    [styles] 优化布局

---
+++ src/Uestc.Course.Core/Entities/UserEntity.cs
@@ -0,0 +1,71 @@
+﻿namespace Uestc.Course.Core.Entities
+{
+    /// <summary>
+    /// 用户实体
+    /// </summary>
+    public class UserEntity
+    {
+        #region 账户信息（用户名、注册邮箱、密码、头像、注册时间）
+        /// <summary>
+        /// ID
+        /// </summary>
+        public string Id { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 邮箱
+        /// </summary>
+        public string Email { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 头像
+        /// </summary>
+        public string Avatar { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Password { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 注册时间
+        /// </summary>
+        public DateTime RegisterTime { get; set; } = DateTime.Now;
+        #endregion
+
+        #region 权限信息（账户状态、角色、API访问控制、令牌）
+        /// <summary>
+        /// 账户状态
+        /// </summary>
+        public UserState State { get; set; } = UserState.Checking;
+
+        /// <summary>
+        /// 用户角色
+        /// </summary>
+        public UserRole Role { get; set; } = UserRole.User;
+        #endregion
+    }
+
+    /// <summary>
+    /// 用户角色
+    /// </summary>
+    public enum UserRole
+    {
+        User = 0, // 用户
+        System // 系统
+    }
+
+    /// <summary>
+    /// 账户状态
+    /// </summary>
+    public enum UserState
+    {
+        Checking = 0, // 待验证
+        Normal, // 正常
+        Forbidden // 封禁中
+    }
+}
---
+++ src/Uestc.Course.Core/Helpers/EncryptAndDecryptHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Uestc.Course.Core.Helpers
+{
+    public static class EncryptAndDecryptHelper
+    {
+        /// <summary>
+        /// Create MD5 hash for the content.
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public static string CreateMD5(this string content)
+        {
+            return BitConverter
+                .ToString(MD5.HashData(Encoding.UTF8.GetBytes(content)))
+                .Replace("-", string.Empty);
+        }
+    }
+}
